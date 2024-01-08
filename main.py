c1c7cfbb05d7b87564b0e8bbcaba09ff6e89ceef
gazevedo9962 <gv52878@gmail.com>
2024-01-08 12:29:30 -0300

    Mon Jan  8 12:29:29     2024

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from pydantic import BaseModel
 from extract import *
 import os
 
-
 SECRET = os.getenv("SECRET")
-
-#
 app = FastAPI()
 
+#def class ...
 class Msg(BaseModel):
     msg: str
     secret: str
 
-@app.get("/")
+class ModelService(str, Enum):
+    consulta = "consulta"
 
+#def get ...
+@app.get("/")
 async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
-
 
 @app.get("/homepage")
 async def demo_get():
@@ -27,11 +27,20 @@ async def demo_get():
     driver.close()
     return homepage
 
+@app.get("/servicos/{model_service}")
+async def get_service(model_service: ModelService, cadernos: str | int = 0, secoes: str | int = 0):
+    print(ModelService, model_service)
+    if model_service is ModelService.consulta:
+        return {
+                "this is consult ...": model_service,
+                "cadernos": cadernos,
+                "secoes": secoes
+                }
+#def post ...
 @app.post("/backgroundDemo")
 async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
     
     background_tasks.add_task(doBackgroundTask, inp)
     return {"message": "Success, background task started"}
-    
 
 
