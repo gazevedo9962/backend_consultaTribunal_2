3ce9a941d1cc38bf3dc9950ecd00411579fd132b
gazevedo9962 <gv52878@gmail.com>
2024-01-10 15:07:44 -0300

    Wed Jan 10 15:07:44     2024

--- extract.py
+++ extract.py
@@ -44,8 +44,7 @@ def getTjsp_teste(driver: webdriver.Chrome, options_values) -> str:
     sleep(0.5)
     #select option cadernos ...
     select_cadernos = Select(driver.find_element(By.ID, "cadernos"))
-    print(int(options_values['cadernos']), len(select_cadernos.options))
-    if [ int(options_values['cadernos']) < len(select_cadernos.options) ]:
+    if int(options_values['cadernos']) < len(select_cadernos.options):
         select_cadernos.select_by_value(options_values['cadernos'])
     else:
         select_cadernos.select_by_value(0)    
@@ -59,7 +58,7 @@ def getTjsp_teste(driver: webdriver.Chrome, options_values) -> str:
     sleep(0.5)
     #select option secao ...
     select_secoes = Select(driver.find_element(By.ID, "secoes"))
-    if [ int(options_values['secoes']) < len(select_secoes.options) ]:
+    if int(options_values['secoes']) < len(select_secoes.options):
         select_secoes.select_by_value(options_values['secoes'])
     else:
         select_secoes.select_by_value(0)
