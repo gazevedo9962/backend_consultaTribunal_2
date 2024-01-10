f616f6babb112899487e402f60c81ee817600f94
gazevedo9962 <gv52878@gmail.com>
2024-01-10 19:03:27 -0300

    Wed Jan 10 19:03:27     2024

--- req_test3.js
+++ req_test3.js
@@ -6,7 +6,7 @@ const command = require("./command")
 // "http://localhost:6546/"
 
 function getdata () {
-  axios.get("https://fastapi-selenium-production-30d6.up.railway.app/tjsp/servicos/consulta/cadernos", {
+  axios.get("https://fastapi-selenium-production-30d6.up.railway.app/tjsp/servicos/consulta/secoes", {
     params: {
         "cadernos": argv[2] || 0,
         "secoes": argv[3] || 0
