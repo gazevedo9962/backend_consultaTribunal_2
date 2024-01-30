f97d7a52ca52711d53919994d5298a05616e20cf
GabrielAzevedo12 <gazevedo996@gmail.com>
2024-01-29 22:04:59 -0300

    seg 29 jan 2024 22:04:59 -03

--- main.py
+++ main.py
@@ -113,10 +113,10 @@ async def get_cadernos_secoes(request: Request, model_service: ModelService):
         "cadernos": request.query_params["cadernos"] or 0,
         "secoes": request.query_params["secoes"] or 0
         }
-    with open("./dados/json/cadernos.json") as arquivo:
-        cadernos = json.load(arquivo)
-    with open("./dados/json/secao.json") as arquivo:
-        secoes = json.load(arquivo)
+    with open("./dados/json/cadernos.json") as arquivo1:
+        cadernos = json.load(arquivo1)
+    with open("./dados/json/secao.json") as arquivo2:
+        secoes = json.load(arquivo2)
 
     data = { "list_cadernos": cadernos[int(options_values["cadernos"])], "list_secoes": secoes[int(options_values["secoes"])] }
 
