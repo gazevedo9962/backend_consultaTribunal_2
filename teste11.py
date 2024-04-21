6cd22225d48dc7f511a0b1c5610461fed00b7fb2
GabrielAzevedo12 <gazevedo996@gmail.com>
2024-04-21 15:12:46 -0300

    ...

--- teste11.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("./dados/json/cadernos.json") as arquivo1:
-    cadernos = json.load(arquivo1)
-with open("./dados/json/secao.json") as arquivo2:
-    secoes = json.load(arquivo2)  
-data = { "list_cadernos": cadernos[int(0)], "list_secoes": secoes[int(0)] }
-
-print(data)
