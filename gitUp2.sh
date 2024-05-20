81cf05ed945f7b1b4b48248359df1521ba0e427f
gazevedo9962 Azevedo <gv52878@gmail.com>
2024-05-20 12:37:47 +0000

    first commit

---
+++ gitUp2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gazevedo9962/backend_consultaTribunal_2.git
+git push -u origin main
